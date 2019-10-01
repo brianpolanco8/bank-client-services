285bd720917ecd0024b5f9ab118dddf30f65de07
brianpolanco <brianf.baez8@gmail.com>
2019-10-01 14:06:37 -0400

    change port number

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const handleSelect = async (tblName) => {
 }
 
 
-const PORT = 5000 || process.env.PORT
+const PORT = 3000 || process.env.PORT
 
 app.listen(PORT, () => {
     console.log('server running on port 5000')
