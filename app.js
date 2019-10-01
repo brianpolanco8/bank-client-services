277e2eabcf60901d1e3829c2828c09c5c42d4efe
brianpolanco <brianf.baez8@gmail.com>
2019-10-01 16:54:22 -0400

    added .env file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const sequelize = require('./data')
 const ejs = require('ejs')
 const routes = require('./routes')
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(routes)
--- data.js
+++ data.js
@@ -1,11 +1,9 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize('DB_CLIENT_SERVICES', 'root', 'root', {
-    host: '127.0.0.1',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    username: 'root',
-    password: 'root'
 });
 
 // const sequelize = new Sequelize('DB_CLIENT_SERVICES', 'brianpolanco', 'Strongpassword12', {
