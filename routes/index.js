7b40eeea05e838392d564d7e2aeec439e7694ef8
brianpolanco <brianf.baez8@gmail.com>
2019-10-01 14:17:41 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers')
 
-// router.get('/', controllers.getHomePage)
-router.get('/', (req, res) => {
-    res.send({ working: "yes" })
-})
+router.get('/', controllers.getHomePage)
 router.get('/clients', controllers.getClientsPage)
 router.get('/requests', controllers.getRequestsPage)
 router.post('/requests', controllers.postRequests)
